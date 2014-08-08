58df5a5925699ae285adb339ccb645dc6ec5a3e4
Nate West <nwest@detroitlabs.com>
2014-08-08 15:30:04 -0400

    Initial Commit

---
+++ Notch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notch
+//
+//  Created by Nate West on 8/8/14.
+//  Copyright (c) 2014 Nate West. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
