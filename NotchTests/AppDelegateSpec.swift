d474cfaa86cfc1f4503b70e5e2c16be32d4119bf
Nate West <nwest@detroitlabs.com>
2014-08-17 15:04:07 -0400

    tweak optionals

--- NotchTests/AppDelegateSpec.swift
+++ NotchTests/AppDelegateSpec.swift
@@ -4,28 +4,27 @@ import Nimble
 class AppDelegateSpec: QuickSpec {
     override func spec() {
         describe("AppDelegate") {
-            
-            var appDelegate: AppDelegate?
+            var appDelegate: AppDelegate!
             
             beforeEach {
                 appDelegate = AppDelegate()
             }
             
             it("has an Application Documents Directory") {
-                expect(appDelegate?.applicationDocumentsDirectory).toNot(beNil())
+                expect(appDelegate.applicationDocumentsDirectory).toNot(beNil())
             }
             
             it("has a Managed Object Model") {
-                expect(appDelegate?.managedObjectContext).toNot(beNil())
+                expect(appDelegate.managedObjectContext).toNot(beNil())
             }
             
             it("has a Persistent Store Coordinator") {
-                expect(appDelegate?.persistentStoreCoordinator).toNot(beNil())
+                expect(appDelegate.persistentStoreCoordinator).toNot(beNil())
             }
             
             it("allows you to save the context") {
                 expect {
-                    appDelegate?.respondsToSelector(Selector("saveContext")) as Bool!
+                    appDelegate.respondsToSelector(Selector("saveContext")) as Bool!
                 }.to(beTruthy())
             }
         }
