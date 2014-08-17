94094af312d6b24bf39f0fc2cdc58c7d881145ae
Nate West <nwest@detroitlabs.com>
2014-08-17 14:19:32 -0400

    remove some boilerplate and add some tests

--- NotchTests/AppDelegateSpec.swift
+++ NotchTests/AppDelegateSpec.swift
@@ -3,10 +3,30 @@ import Nimble
 
 class AppDelegateSpec: QuickSpec {
     override func spec() {
-        describe("a BDD style test in Swift") {
-            it("works!") {
-                let isThisWorking = true
-                expect(isThisWorking).to(beTruthy())
+        describe("AppDelegate") {
+            
+            var appDelegate: AppDelegate?
+            
+            beforeEach {
+                appDelegate = AppDelegate()
+            }
+            
+            it("has an Application Documents Directory") {
+                expect(appDelegate?.applicationDocumentsDirectory).toNot(beNil())
+            }
+            
+            it("has a Managed Object Model") {
+                expect(appDelegate?.managedObjectContext).toNot(beNil())
+            }
+            
+            it("has a Persistent Store Coordinator") {
+                expect(appDelegate?.persistentStoreCoordinator).toNot(beNil())
+            }
+            
+            it("allows you to save the context") {
+                expect {
+                    appDelegate?.respondsToSelector(Selector("saveContext")) as Bool!
+                }.to(beTruthy())
             }
         }
     }
